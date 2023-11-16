417e33d2cf45a333aab1c1cf556ed015940f8a7b
G12251 <gulnozasaparmatova@gmail.com>
2023-11-16 14:15:14 +0500

    thermometer

--- main.js
+++ main.js
@@ -41,3 +41,66 @@ function press() {
 }
 
 document.getElementById("led").addEventListener("click", press)
+
+FusionCharts.ready(function () {
+    var chart = new FusionCharts({
+        type: 'thermometer',
+        renderAt: 'chart-container',
+        id  : 'myThm',
+        width: '240',
+        height: '300',
+        dataFormat: 'json',
+        dataSource: {
+            "chart": {
+                "caption": "Temperature Monitor",
+                "subcaption": " Central cold store",
+                "lowerLimit": "-20",
+                "upperLimit": "20",
+                "numberSuffix": "°C",
+                "showhovereffect": "1",
+                "thmFillColor": "#008ee4",
+                "showGaugeBorder" : "1",
+                "gaugeBorderColor" :  "#008ee4",
+                "gaugeBorderThickness" :  "2",
+                "gaugeBorderAlpha" :  "30",
+                "thmOriginX": "100",
+                "theme" : "fint"
+            },
+            "value": "-6",
+            //All annotations are grouped under this element
+            "annotations": {
+                "showbelow": "0",
+                "groups": [                        
+                    {                  
+                        //Each group needs a unique ID
+                        "id": "indicator",
+                        "items": [
+                            //Showing Annotation
+                            {
+                                "id": "background",
+                                //Polygon item 
+                                "type": "rectangle",
+                                "alpha" : "50",
+                                "fillColor": "#AABBCC",           
+                                "x" : "$gaugeEndX-35",
+                                 "tox" : "$gaugeEndX",
+                                "y" : "$gaugeEndY+55",
+                                "toy" : "$gaugeEndY+72"
+                            }
+                        ]
+                    }
+                ]
+                
+            },
+        },
+        "events" :{
+            "rendered" : function (evt, arg) {
+                var chargeInterval = setInterval( function(){
+                    var temp = parseInt(Math.random()*2) -5;
+                    FusionCharts.items["myThm"].feedData("&value="+temp);
+                }, 4000);
+            }   
+        }
+    })
+    .render();
+});
